7e3e692675efa1516f30a84aea4a183c9b8a01e5
Toshin Prince <toshinprince@gmail.com>
2024-05-24 13:22:58 +0530

    Test: Integration Test

--- test/unit/FundMeTest.t.sol
+++ test/unit/FundMeTest.t.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
-import {FundMe} from "../src/FundMe.sol";
-import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
@@ -134,7 +134,7 @@ contract FundMeTest is Test {
             //vm.prank new address
             //vm.deal new address
             // address()
-            //hoax is foege standard library which equals vm.prank + vm.deal
+            //hoax is forge standard library which equals vm.prank + vm.deal
 
             hoax(address(i), SEND_VALUE);
             fundMe.fund{value: SEND_VALUE}();
